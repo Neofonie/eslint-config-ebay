5dee8cbafd5aa488e9d168777066e1e4d05bc3bd
Michael Veeck <veeck@neofonie.de>
2018-06-29 18:56:53 +0200

    Run eslint fix on its own

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 'use strict';
 
 module.exports = {
-    "extends": [
+    'extends': [
         'eslint-config-neofonie/legacy',
         'eslint-config-neofonie/rules/es6'
     ].map(require.resolve),
     parserOptions: {
         ecmaVersion: 2017
     },
-    "rules": {}
+    'rules': {}
 };
--- legacy.js
+++ legacy.js
@@ -1,20 +1,20 @@
 'use strict';
 
 module.exports = {
-    "extends": [
+    'extends': [
         'eslint-config-neofonie/rules/bp',
         'eslint-config-neofonie/rules/errors',
         'eslint-config-neofonie/rules/node',
         'eslint-config-neofonie/rules/style',
         'eslint-config-neofonie/rules/variables'
     ].map(require.resolve),
-    "rules": {},
-    "env": {
-        "browser": true,
-        "node": true,
-        "amd": false,
-        "mocha": true,
-        "jasmine": false,
-        "jquery": true
+    'rules': {},
+    'env': {
+        'browser': true,
+        'node': true,
+        'amd': false,
+        'mocha': true,
+        'jasmine': false,
+        'jquery': true
     }
 };
